ca6c60bfa8357382745ea5cb82e4237ae52d2fc1
IbrahimWicaksono2002 <wicaksonoibrahim334@gmail.com>
2023-05-11 17:47:21 +0700

    UTS

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import pengguna.Mhs;
+
 class Main {
     public static void main(String[] args) {
+        Mhs mhs = new Mhs("ini_username", "ini_password");
+
+        mhs.setNama("Tauhid Ibrahim Wicaksono");
+        mhs.setNim("G.111.20.0004");
+        mhs.setSmt(6);
+
+        System.out.println("Nama Mhs: " + mhs.getNama());
+        System.out.println("Nim Mhs: " + mhs.getNim());
+        System.out.println("Smt Mhs: " + mhs.getSmt());
+        System.out.println("Username: " + mhs.getUsername());
+        System.out.println("Password: " + mhs.getPassword());
+
 
     }
 }
